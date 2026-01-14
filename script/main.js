4091bc029199a38f7e8469fa1c6f793ae56f44b0
jaewon Ahn <hyo4754@gmail.com>
2026-01-14 22:33:49 +0900

    포트폴리오 섹션 - Main, Other 완료, Widget 구현 시작

--- script/main.js
+++ script/main.js
@@ -8,10 +8,20 @@ const page = {
     const tabItems = document.querySelectorAll(".portfolio__main-item");
 
     tabItems.forEach((tabItem, index) => {
-      tabItem.classList.toggle("isactive", index === 0);
+      tabItem.classList.toggle("is-active", index === 0);
     });
     tabs.forEach((tab, index) => {
       tab.classList.toggle("is-active", index === 0);
+    });
+
+    tabs.forEach((tab, index) => {
+      tab.addEventListener("click", () => {
+        tabs.forEach((t) => t.classList.remove("is-active"));
+        tab.classList.add("is-active");
+
+        tabItems.forEach((tabItem) => tabItem.classList.remove("is-active"));
+        tabItems[index].classList.add("is-active");
+      });
     });
   },
 };
